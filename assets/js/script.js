c7c825633e4353dc3d7cf85c2448801f8f388648
Chelsea Smith <smith2chelsea2@gmail.com>
2021-05-08 23:05:53 -0400

    condtions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,92 @@
 // Assignment Code
 
 //spec character array
-var specChar = [",", "." , "<", ">", "/", "?", "'", ";", ":","[", "]", "{", "}", "|", "`" , "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+"];
+var specChar = "!@#$%^&*()_-+={}[]\|,.<>/?".split("");
 
 // number array
-var numbArray = ["1","2","3","4","5","6","7","8","9","0"];
+var numbArray = "123456789".split("");
 
 // lower case array
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lowerCase = "abcdefghjklmnopqrstuvwxyz".split("");
 
 // upper case array 
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-// var to store length of password
-var length = parseInt (
-  prompt("How many charcters would you like your password to be?")
-)
+function userPrompt() {
+  var options = {};
 
-//conditional to check if parse is a number 
-if (Number.isNaN(length)) {
-  alert("Password must be a number");
-  return;
+  // var to store length of password
+  var length = parseInt(
+    prompt("How many charcters would you like your password to be?"));
+  length = parseInt(length);
+
+  //conditional to check if parse is a number 
+  if (Number.isNaN(length)) {
+    alert("Password must be a number");
+    return null;
+  }
+
+  // at least 8 characters conditional  
+  if (length < 8) {
+    alert("Password length must be 8 characters");
+    return null;
+  }
+
+  // at most 128 character conditional
+  if (length > 128) {
+    alert("Password must not contain more than 128 characters");
+    return null;
+  }
+
+  options.length = length ;
+
+  var special = prompt(
+    "Do you want to use special charcaters (such as *&?$/...) Yes or No"
+  );
+
+  special = special.toUpperCase ();
+
+  if (special !== "Yes"  && special !== "No") {
+    alert("Please enter Yes or No");
+    return null;
+  } 
+    else if (special === "Yes"){
+      options.specChar = specChar;
+    }
+
+  }
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var generateBtn = document.querySelector("#generate");
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
