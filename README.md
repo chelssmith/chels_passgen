4acf9da43e751910d332b7873b7ad97aa7c5acb3
Chelsea Smith <smith2chelsea2@gmail.com>
2021-05-12 08:57:33 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# chels_passgen
+# Password Generator
+
+This password generator allows the user to create a randomized password compiled of numbers, upper and lower case letters, and special characters.
+
+The password generator will allows the user have a chosen length between 8 and 128 characters.
+
+
+
+# Methods
+This project was created using javascript.
+
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,25 @@
 // Assignment Code
 
 //spec character array
-var specChar = "!@#$%^&*()_-+={}[]\|,.<>/?".split("");
-
+var specChar = "!@#$%^&*()_-+={}[]/|,.<>/?".split("");
 // number array
 var numbArray = "123456789".split("");
-
 // lower case array
 var lowerCase = "abcdefghjklmnopqrstuvwxyz".split("");
-
 // upper case array 
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-function userPrompt() {
-  var options = {};
 
   // var to store length of password
-  var length = parseInt(
-    prompt("How many charcters would you like your password to be?"));
-  length = parseInt(length);
-
+  var wordLength = function () {
+    var length = window.prompt("How many charcters would you like your password to be?");
+  
   //conditional to check if parse is a number 
   if (Number.isNaN(length)) {
     alert("Password must be a number");
     return null;
   }
-
-  // at least 8 characters conditional  
-  if (length < 8) {
-    alert("Password length must be 8 characters");
-    return null;
-  }
-
-  // at most 128 character conditional
-  if (length > 128) {
-    alert("Password must not contain more than 128 characters");
-    return null;
-  }
-
-  options.length = length;
-
-  var special = prompt("Do you want to use special charcaters (such as *&?$/...) Yes or No");
-
-  special = special.toUpperCase();
+  var special = prompt("Do you want to use special charcaters (such as *&@#?$/...) Yes or No");
 
   if (special !== "Yes" && special !== "No") {
     alert("Please enter Yes or No");
@@ -51,64 +28,57 @@ function userPrompt() {
   else if (special === "Yes") {
     options.specChar = specChar;
   }
-  else {}
 
   var upper = prompt("Do you want to use uppercase letters Yes or No");
 
-  upper = upper.toUpperCase();
-
-  if (upper !== "Yes" && upper !== "No"){
+  if (upper !== "Yes" && upper !== "No") {
     alert("Please enter Yes or No");
     return null;
   }
   else if (upper === "Yes") {
     options.upperCase = upperCase;
   }
-  else {}
+
 
   var lower = prompt("Do you want to use lowercase letters Yes or No");
 
-  lower = lower.toUpperCase();
-
-  if (lower !== "Yes" && lower !== "No"){
+  if (lower !== "Yes" && lower !== "No") {
     alert("Please enter Yes or No");
     return null;
   }
-  else if (lower === "Yes"){
+  else if (lower === "Yes") {
     options.lowerCase = lowerCase
   }
 
 
+  var number = prompt("Do you want to use numbers Yes or No");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  var generateBtn = document.querySelector("#generate");
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
+  if (number !== "Yes" && number !== "No") {
+    alert("Please enter Yes or No");
+    return null;
   }
+  else if (number === "Yes") {
+    options.numbArray = numbArray
+  }
+ 
+  return options;
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+};
+
+
+//other code before this
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
